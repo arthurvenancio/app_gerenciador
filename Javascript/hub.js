f61f763c5af15d0117c632334fd6b8a53ac15290
Arthur Venâncio <arthurmvenancio@gmail.com>
2023-04-13 23:24:35 -0300

    função de atualizar o tempo ainda com bug

--- Javascript/hub.js
+++ Javascript/hub.js
@@ -3,6 +3,7 @@ function Atividade(titulo,equipamento,tempo_estimado){
     this.equipamento=String(equipamento);
     this.tempo_estimado=parseFloat(tempo_estimado);
     
+    this.duracao=0;
     this.status="Planejado";
     this.criacao=Date.now();
 
@@ -13,15 +14,7 @@ function Atividade(titulo,equipamento,tempo_estimado){
         this.status="Em andamento"
     }
 }
-/*
-<a href="#" class="atividade">
-                <h2>Título Atividade</h2>
-                <div class="atividade_status">
-                    <p>Tempo de Atividade</p>
-                    <p>Status da Atividade</p>
-                </div>
-            </a>
-*/
+
 function adicionar_atividade(atividade){
     //Criando o elemento mais externo
     let elemento=document.createElement('a')
@@ -37,7 +30,7 @@ function adicionar_atividade(atividade){
     let atividade_status=document.createElement('div')
     let tempo_de_atividade=document.createElement('p')
     let status=document.createElement('p')
-    //Colocando classes
+    //Colocando classe
     atividade_status.classList.add('atividade_status')
     tempo_de_atividade.classList.add('duracao_atividade')
     //Estabelecendo parentesco
@@ -45,12 +38,39 @@ function adicionar_atividade(atividade){
     atividade_status.appendChild(tempo_de_atividade)
     atividade_status.appendChild(status)
     //Iniciando valores
-    tempo_de_atividade.innerHTML='0 s'
+    tempo_de_atividade.innerHTML=`${atividade.duracao}s`
     status.innerHTML=atividade.status
 
 }
+
+let lista_atividades=[]
+
 document.querySelector('#nova_atividade').addEventListener('click',()=>{
     let atividade = new Atividade("Atividade Teste","Equipamento 1",30)
+    lista_atividades.push(atividade)
     adicionar_atividade(atividade)
     }
 )
+
+function atualizar_tempo(lista){
+    let elementos=document.querySelectorAll('.duracao_atividade')
+    for(let atividade of lista){
+        let duracao=Math.floor((Date.now()-atividade.criacao)/1000)
+        if(duracao<60){
+            atividade.duracao=`${duracao}s`
+        }
+        else if(duracao>=60 && duracao<(60*60)){
+            let minutos=Math.floor(duracao/60)
+            let segundos=duracao%60
+            atividade.duracao=`${minutos}:${segundos}`
+        }
+        else if(duracao>=(60*60)){
+            let horas=Math.floor(duracao/(60*60))
+            minutos=Math.floor(duracao%(60*60))
+            atividade.duracao=`${horas}:${minutos}`
+        }
+        elementos[lista.indexOf(atividade)].innerHTML=atividade.duracao
+    }
+}
+
+setInterval(atualizar_tempo(lista_atividades),1000)
